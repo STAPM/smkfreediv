08f53a657eab4e202f0bf8e1cab0ed7355eafb97
Damon Morris <d.j.morris@sheffield.ac.uk>
2021-11-17 13:40:09 +0000

    bug fixes

--- R/CalcWeekSpend.R
+++ R/CalcWeekSpend.R
@@ -45,8 +45,8 @@ CalcWeekSpend <- function(data,
   mean_calcs[, sample := ifelse(is.na(weekspend), 0, 1)]
 
   data_out <- mean_calcs[, .(mean_week_spend = weighted.mean(weekspend, w = Aweight0 ,na.rm = T),
-                             se_week_spend = sqrt(spatstat::weighted.var(weekspend, na.rm = T)) / sqrt(.N),
-                             median_week_spend = spatstat::weighted.median(weekspend, w = Aweight0, na.rm = T),
+                             se_week_spend = sqrt(spatstat.geom::weighted.var(weekspend, w = Aweight0,na.rm = T)) / sqrt(.N),
+                             median_week_spend = spatstat.geom::weighted.median(weekspend, w = Aweight0, na.rm = T),
                              sample_tkit = sum(sample)
                 ),
             by = strat_vars]
